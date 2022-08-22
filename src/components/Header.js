6c71c441f8a233b5915c3ec45e4e94fdea5c4e5e
jsm8109jsm <jsm8109jsm@gmail.com>
2022-08-22 14:27:04 +0900

    FEAT :: SignUp Login Toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,37 @@ import "../styles/Header.css";
 import SignUp from "./SignUp";
 
 function Header() {
-  const [signUp, setSignUp] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
   const onClick = () => {
-    setSignUp((prev) => !prev);
+    setModal((prev) => !prev);
+  };
+  const changeType = () => {
+    setIsSignUp((prev) => !prev);
   };
   return (
     <div className="Header">
       <span
         onClick={() => {
-          setSignUp(true);
+          setIsSignUp(true);
+          setModal(true);
         }}
       >
         회원가입
       </span>
-      {signUp && <SignUp onClick={onClick} />}
+      <span
+        onClick={() => {
+          setIsSignUp(false);
+          setModal(true);
+        }}
+      >
+        로그인
+      </span>
+      {/* {signUp && <SignUp onClick={onClickSignUp} />}
+      {login && <Login onClick={onClickLogin} />} */}
+      {modal && (
+        <SignUp onClick={onClick} changeType={changeType} type={isSignUp} />
+      )}
     </div>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,67 +2,131 @@ import React, { useState } from "react";
 import "../styles/SignUp.css";
 
 function SignUp(props) {
-  const { onClick } = props;
-  console.log("asdf");
-  const [inputs, setInputs] = useState({
+  const { onClick, changeType, type } = props;
+  const [signUpInputs, setSignUpInputs] = useState({
     name: "",
     nickname: "",
     email: "",
     password: "",
   });
-  const { name, nickname, email, password } = inputs;
-  const onChange = (e) => {
+  const [loginInputs, setLoginInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeTypeInModal = () => {
+    changeType();
+    type
+      ? setSignUpInputs({
+          name: "",
+          nickname: "",
+          email: "",
+          password: "",
+        })
+      : setLoginInputs({
+          email: "",
+          password: "",
+        });
+  };
+
+  const onChangeSignUp = (e) => {
     const { name, value } = e.target;
     const nextInputs = {
-      ...inputs,
+      ...signUpInputs,
       [name]: value,
     };
-    setInputs(nextInputs);
+    setSignUpInputs(nextInputs);
   };
+
+  const onChangeLogin = (e) => {
+    const { name, value } = e.target;
+    const nextInputs = {
+      ...loginInputs,
+      [name]: value,
+    };
+    setLoginInputs(nextInputs);
+  };
+
   const signUp = () => {
-    console.log(inputs);
+    console.log(signUpInputs);
   };
+  const Login = () => {
+    console.log(loginInputs);
+  };
+
   return (
     <div className="SignUp" onClick={onClick}>
-      <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
-        <h1 className="SignUp-title">회원가입</h1>
-        <div>
-          <input
-            name="name"
-            placeholder="이름"
-            value={name}
-            onChange={(e) => onChange(e)}
-            className="SignUp-input"
-          />
-          <input
-            name="nickname"
-            placeholder="닉네임"
-            value={nickname}
-            onChange={(e) => onChange(e)}
-            className="SignUp-input"
-          />
-          <input
-            name="email"
-            placeholder="이메일주소"
-            value={email}
-            onChange={(e) => onChange(e)}
-            className="SignUp-input"
-          />
-          <input
-            name="password"
-            placeholder="비밀번호"
-            value={password}
-            onChange={(e) => onChange(e)}
-            className="SignUp-input"
-          />
+      {type ? (
+        <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
+          <h1 className="SignUp-title">회원가입</h1>
+          <div>
+            <input
+              name="name"
+              placeholder="이름"
+              value={signUpInputs.name}
+              onChange={(e) => onChangeSignUp(e)}
+              className="SignUp-input"
+            />
+            <input
+              name="nickname"
+              placeholder="닉네임"
+              value={signUpInputs.nickname}
+              onChange={(e) => onChangeSignUp(e)}
+              className="SignUp-input"
+            />
+            <input
+              name="email"
+              placeholder="이메일주소"
+              value={signUpInputs.email}
+              onChange={(e) => onChangeSignUp(e)}
+              className="SignUp-input"
+            />
+            <input
+              name="password"
+              placeholder="비밀번호"
+              value={signUpInputs.password}
+              onChange={(e) => onChangeSignUp(e)}
+              className="SignUp-input"
+            />
+          </div>
+          <div>
+            <button className="SignUp-button" onClick={() => signUp()}>
+              가입하기
+            </button>
+            <button className="SignUp-button" onClick={() => changeTypeInModal()}>
+              로그인
+            </button>
+          </div>
         </div>
-        <div>
-          <button className="SignUp-button" onClick={() => signUp()}>
-            가입하기
-          </button>
-          <button className="SignUp-button">로그인</button>
+      ) : (
+        <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
+          <h1 className="Login-title">로그인</h1>
+          <div>
+            <input
+              name="email"
+              placeholder="이메일주소"
+              value={loginInputs.email}
+              onChange={(e) => onChangeLogin(e)}
+              className="Login-input"
+            />
+            <input
+              name="password"
+              placeholder="비밀번호"
+              value={loginInputs.password}
+              onChange={(e) => onChangeLogin(e)}
+              className="Login-input"
+            />
+          </div>
+          <div>
+            <button className="Login-button" onClick={() => Login()}>
+              로그인
+            </button>
+            <button className="Login-button" onClick={() => changeTypeInModal()}>
+              회원가입
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
