61b0ac784c5d7a02dd1c79d78b1b00932bf8926d
jsm8109jsm <jsm8109jsm@gmail.com>
2022-09-10 17:14:57 +0900

    ADD :: NotFound

---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header';
+import "../styles/NotFound.css";
+
+function NotFound() {
+  return (
+    <div>
+      <Header />
+      <div className='NotFound'>
+        <span className='NotFound-title'>404 Error</span>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound;
