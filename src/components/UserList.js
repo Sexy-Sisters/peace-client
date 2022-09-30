08dd50f8b9b5b75ef8b933f2b4e83faf43b9430b
jsm8109jsm <jsm8109jsm@gmail.com>
2022-09-30 22:27:47 +0900

    FEAT : UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,12 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { ImMusic } from 'react-icons/im';
 import { instance } from '../instance/instance';
 import Header from './Header';
+import '../styles/UserList.scss';
+
+function UserListList() {
+  return (
+    <div>
+      wow
+    </div>
+  )
+}
 
 function UserList() {
-  const [userList, setUserList] = useState();
+  const [userList, setUserList] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const getUserList = async () => {
       try {
+        setLoading(true);
         const response = await instance.get('user');
         setUserList(response.data);
       } catch (error) {
@@ -15,15 +26,21 @@ function UserList() {
       }
     }
     getUserList();
+    setLoading(false);
   }, []);
   return (
     <>
       <Header />
-      <div className='PlayList'>
+      <div className='UserList'>
         <div className="Chart-title">
           <ImMusic className="title-icon" size={60} />
           <div className="title-vertical"></div>
           <h1 className="title">PLAYLIST</h1>
+          <div className='UserList-list'>
+            {!loading ? userList.map((item) => {
+              return <UserListList data={item} key={item.id}></UserListList>
+            }) : <>로딩중~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</>}
+          </div>
         </div>
       </div>
     </>
